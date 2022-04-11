3364607dd2e1137bf65046d00dc403a64d017d77
Adriano Santos <adrianofrontend@gmail.com>
2022-04-11 18:33:14 -0300

    Criando a interface da aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,85 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    </div>
-    <router-view/>
+    <TheHeader />
+    <main id="main">
+      <router-view />
+    </main>
+    <TheFooter />
   </div>
 </template>
 
-<style>
+<script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
 
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap");
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Open Sans", Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+body,
+ul,
+li,
+h2,
+p {
+  padding: 0;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  color: white;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Open Sans", Arial, Helvetica, sans-serif;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main{
+  flex: 1;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>  👍Compre ou Venda 👎</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
 export default {
-  name: "Home",
+  name: "home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
